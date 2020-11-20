3b97b58716d60716f2c46cf4ae7685c75a3f6e4b
Robert Rentea <rentea.robert790@yahoo.com>
2020-11-20 15:43:30 +0200

    added syscall cases

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -15,6 +15,46 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  int syscall_no = ((int*)f->esp)[0];
+	int fd, no;
+	char *buf;
+	int waited_tid;
+	int *status, th_status, i;
+
+
+	switch (syscall_no) {
+    case SYS_HALT:
+      break;
+    case SYS_EXIT:
+      break;
+    case SYS_EXEC:
+      break;
+    case SYS_WAIT:
+      break;
+    case SYS_CREATE:
+      break;
+    case SYS_REMOVE:
+      break;
+    case SYS_OPEN:
+      break;
+    case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      fd = ((int*)f->esp)[1];
+      buf = (char*)((int*)f->esp)[2];
+      no = ((int*)f->esp)[3];
+      break;
+    case SYS_SEEK:
+      break;
+    case SYS_TELL:
+      break;
+    case SYS_CLOSE:
+      break;
+  }
   thread_exit ();
 }
+
+
+
